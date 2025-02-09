0966b579f67dae6de032092d1d7d614faf1a93ef
matthew-deal-rocketlab <120158394+matthew-deal-rocketlab@users.noreply.github.com>
2025-02-10 00:04:28 +1100

    [lesson]: added lesson for image optimization

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,11 @@
-import type { MDXComponents } from "mdx/types";
-import CodeBlock from "@/components/CodeBlock";
-import Demo from "@/components/Demo";
-import Alert from "@/components/alert";
-import { InteractiveExercises } from "@/components/interactive-exercises";
-import ServerClientGuide from "@/components/server-client-guide";
-import ThemeImage from "@/components/ThemeImage";
+import type { MDXComponents } from 'mdx/types'
+import CodeBlock from '@/components/CodeBlock'
+import Demo from '@/components/Demo'
+import Alert from '@/components/alert'
+import { InteractiveExercises } from '@/components/interactive-exercises'
+import ServerClientGuide from '@/components/server-client-guide'
+import ThemeImage from '@/components/ThemeImage'
+import Image from 'next/image'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
@@ -14,6 +15,8 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     InteractiveExercises: InteractiveExercises,
     ServerClientGuide: ServerClientGuide,
     ThemeImage: ThemeImage,
+    Image: Image,
+
     ...components,
-  };
+  }
 }
--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,28 @@
-import createMDX from "@next/mdx";
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
+import type { NextConfig } from 'next'
 
 const withMDX = createMDX({
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
   },
-});
+})
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
   experimental: {
     dynamicIO: true,
   },
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-};
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+}
 
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
